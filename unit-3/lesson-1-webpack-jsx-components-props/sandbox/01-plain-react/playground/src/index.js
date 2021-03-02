f4c458e54c0f4657d45de029aef010b82ee60ef6
Erwins Saget <erwinssaget@gmail.com>
2021-03-02 10:05:35 -0500

    sandbox

---
+++ unit-3/lesson-1-webpack-jsx-components-props/sandbox/01-plain-react/playground/src/index.js
@@ -0,0 +1,5 @@
+const text = "Hello World";
+
+// add the above text to the page using React.createElement
+
+// add a button to the page that logs the text to the console
---
+++ unit-3/lesson-1-webpack-jsx-components-props/sandbox/02-react-babel/playground/src/index.js
@@ -0,0 +1,6 @@
+const text = "Hello World";
+
+// Using JSX
+// add the above text to the page using React.createElement
+
+// add a button to the page that logs the text to the console
