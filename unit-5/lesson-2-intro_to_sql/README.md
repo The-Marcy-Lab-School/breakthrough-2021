64c67b745cbffadfa289824802a866cd64a5b7be
Erwins Saget <erwinssaget@gmail.com>
2021-03-18 12:25:25 -0400

    add sql lesson

---
+++ unit-5/lesson-2-intro_to_sql/README.md
@@ -0,0 +1,47 @@
+# Introduction to Databases & SQL
+
+### Key Terms
+
+- data definition language
+- data manipulation language
+- relational
+- SQL - Structured Query Language
+- schema
+
+database - collection of tables
+tables - collection of records (called rows)
+columns - fields that we can use to identify an entry within a row
+
+### Essential Questions
+
+- What is a relational database?
+- What is a relational database management system (RDBMS)?
+- How are relational and non-relational databases different?
+- What is PostgreSQL?
+- How do we define relations in SQL?
+
+### Learning Assignments
+
+1. [What are Databases & SQL - Video](https://www.khanacademy.org/computing/computer-programming/sql/sql-basics/v/welcome-to-sql) - Watch this two minute video to learn what databases and SQL are, as well as how the tech industry uses them.
+
+2. Launch School Open Book Shelf - [Intro to SQL](https://launchschool.com/books/sql). Read the following sections:
+   - Getting Started Section
+   - Your First Database: Schema
+
+**Review and Bookmark the Following Resources**
+
+- [Postgres Documentation](https://www.postgresql.org/docs/12/index.html) - Bookmark the documentation to Postgres. Read over[Chapter 2 - The SQL Langage](https://www.postgresql.org/docs/12/tutorial-sql.html) to gain an deeper overview of using SQL within Postgres.
+- [SQL Basics Cheat Sheet](https://github.com/treehouse/cheatsheets/blob/master/sql_basics/cheatsheet.md)
+
+**Optional Video Content**
+
+- Watch the below Treehouse Videos to see demos of writing basic SQL statements. Each video is about 3 minutes along and is followed by a brief exercise for you to complete in order to actively follow along with the videos:
+
+  1. [Retrieve All Information](https://teamtreehouse.com/library/your-first-sql-statement)
+  2. [Retrieve Specific Columns of Data](https://teamtreehouse.com/library/retrieving-specific-columns-of-information)
+  3. [Categorize Output](https://teamtreehouse.com/library/categorizing-your-output-with-as)
+  4. [Searching Tables With 'Where'](https://teamtreehouse.com/library/searching-tables-with-where)
+
+### Additional Practice
+
+- [Lesson 4 Practice](./practice/)
--- unit-5/playground/server.js
+++ unit-5/playground/server.js
@@ -1,96 +1,33 @@
-// require express
 const express = require("express");
-const cors = require("./cors");
-
-const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static("public"));
-console.log(cors);
-app.use(cors({ url: "/" }));
+const PORT = 3000;
 
+// template engine
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-const checkHeaders = function (request, response, next) {
-  console.log("running first middleware");
-  const authorization = !!request.header("Authorization");
-  // if (authorization) {
-  return next();
-  // }
+app.use(express.urlencoded());
 
-  // return response.send("not authorized");
-};
+const pets = [];
 
-// GLOBAL MIDDLEWARE
-app.use((req, res, next) => {
-  console.log(`2`);
-  next();
+// /pets
+app.get("/pets", function (req, res) {
+  res.render("pets/index", {
+    pets: pets,
+  });
 });
 
-// ROUTE DEFINITIONS
-
-app.get("/", function (request, response) {
-  // response.sendFile(__dirname + "/public/index.html");
-  response.render("index");
+// SQL
+app.post("/pets", function (req, res) {
+  pets.push(req.body);
+  console.log(req.body);
+  res.render("pets/index", {
+    pets: pets,
+  });
 });
 
-app.use((req, res, next) => {
-  // throw new Error("waaaaahhhh");
-  console.log(`1`);
-  next();
-});
-
-app.get("/contact-us", function (request, response) {
-  response.sendFile(__dirname + "/public/contact.html");
-});
-
-// add /about route
-app.get(
-  "/about",
-  function (req, res, next) {
-    console.log("im running only in about");
-    next();
-  },
-  function (req, res) {
-    const data = req.query;
-
-    res.render("about", {
-      link: data.link,
-      people: ["one", "two", "three"],
-    });
-  }
-);
-
-// /person/2
-app.get("/person/:personId", function (request, response) {
-  console.log(request.params);
-  response.send("Hello Person");
-});
-
-app.get(
-  "/pets/:petId/photos/:photoId?/meta/:metaId",
-  function (request, response) {
-    console.log(request);
-    response.send(request.params);
-  }
-);
-
-app.use((req, res, next) => {
-  console.log("route not found");
-  console.log("---------------------");
-  res.statusCode = "404";
-  res.send("route not found");
-});
-
-app.use((err, req, res, next) => {
-  console.log(err.message);
-
-  res.status(500);
-  res.send("something went wrong");
-});
-
-app.listen(PORT, () => {
-  console.log(`started on http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log("http://localhost:" + PORT);
 });
