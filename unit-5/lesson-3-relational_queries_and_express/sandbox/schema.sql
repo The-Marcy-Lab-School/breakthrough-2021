3d587159a7d93289b45221b64507d1ba420ec24d
Erwins Saget <erwinssaget@gmail.com>
2021-03-23 12:03:23 -0400

    add schema content

---
+++ unit-5/lesson-3-relational_queries_and_express/sandbox/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50)
+);
+ 
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(200),
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+ 
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  contents VARCHAR(240),
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  photo_id INTEGER REFERENCES photos(id) ON DELETE CASCADE
+);
