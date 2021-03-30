05087dec85abc325a7a64f1bd2bb67421da3511e
Erwins Saget <erwinssaget@gmail.com>
2021-03-30 16:34:56 -0400

    add another hook

--- unit-6/lesson-1-custom-hooks/solution/src/components/ChildComponent.js
+++ unit-6/lesson-1-custom-hooks/solution/src/components/ChildComponent.js
@@ -1,18 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 const ChildComponent = (props) => {
-	console.log('CHILD RE_RENDER');
-	useEffect(() => {
-		props.fetchData('users');
-	}, [props]);
-	return (
-		<div>
-			<h2>{props.title}</h2>
-			<p>Child Component!</p>
-			<p>Static Array</p>
-			<p>{props.array}</p>
-		</div>
-	);
+  console.log("CHILD RE_RENDER");
+
+  useEffect(() => {
+    props.fetchData("users");
+  }, [props]);
+
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>Child Component!</p>
+      <p>Static Array</p>
+      <p>{props.array}</p>
+    </div>
+  );
 };
 
 export default React.memo(ChildComponent);
